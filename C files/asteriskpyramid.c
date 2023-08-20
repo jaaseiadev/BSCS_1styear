ec9ba4368f5d917dc3a02523e164685ce39234ba
jaaseiadev <104299697+jaaseiadev@users.noreply.github.com>
2023-08-21 01:01:58 +0800

    update
    adding some space to make it look pyramidiers

--- C files/asteriskpyramid.c
+++ C files/asteriskpyramid.c
@@ -5,7 +5,9 @@ int main() {
 
         for (int i = 1; i <= rows; i++) {
         
-      
+        for(int k = 1; k <= rows - i; k++){
+            printf("_");
+         }
         
         // Print asterisks for the pyramid
         for (int j = 1; j <= 2 * i - 1; j++) {
