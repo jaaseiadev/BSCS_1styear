0d533012542d87feb5ecc3dae15161390589bb54
jaaseiadev <104299697+jaaseiadev@users.noreply.github.com>
2023-08-22 21:47:41 +0800

    new

---
+++ C files/2dArray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    int numbers [2][3] = {
+                            {1,2,3},
+                            {4,5,6}
+                         };
+    
+    printf("%d ", numbers[0][0]);
+    printf("%d ", numbers[0][1]);
+    printf("%d ", numbers[0][2]);
+    printf("\n%d ", numbers[1][0]);
+    printf("%d ", numbers[1][1]);
+    printf("%d ", numbers[1][2]);
+    
+
+
+
+
+
+    return 0;
+}
