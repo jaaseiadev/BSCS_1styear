857bf98926b4ad7de82e7c5aa5c7c4394cdfef5f
jaaseiadev <104299697+jaaseiadev@users.noreply.github.com>
2023-08-22 09:26:22 +0800

    small update
    project euler

---
+++ Project Euler/problem.c
@@ -0,0 +1,19 @@
+#include <stdio.h>>
+
+primefactorial( number){
+    int factorcount = 0;
+
+    for (int i = 2; i <= number; i++){
+            if(number % i == 0 && i % 2,3,4,6,8,9 != 0){
+                factorcount++;
+                printf("\n%d", i);
+            }
+            }
+
+        }
+
+int main(){
+
+primefactorial(13195);
+
+}
